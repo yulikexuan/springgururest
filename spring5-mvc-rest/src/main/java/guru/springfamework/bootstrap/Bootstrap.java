77a61b2a326e2e677270d2fd18c2ae5f76c8d2b4
yulikexuan <yulikexuan@gmail.com>
2018-02-19 13:39:29 -0500

    Add Bootstrap class to load Category data

---
+++ spring5-mvc-rest/src/main/java/guru/springfamework/bootstrap/Bootstrap.java
@@ -0,0 +1,70 @@
+//: guru.springfamework.bootstrap.Bootstrap.java
+
+
+package guru.springfamework.bootstrap;
+
+
+import guru.springfamework.domain.Category;
+import guru.springfamework.domain.repositories.ICategoryRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+
+/*
+ * CommandLineRunner interface used to indicate that a bean should run when it
+ * is contained within a SpringApplication
+ *
+ * Multiple CommandLineRunner beans can be defined within the same application
+ * context and can be ordered using the Ordered interface or @Order annotation
+ *
+ * Command Runner is a Spring Boot specific class of loading data and only going
+ * to work with Spring Boot
+ *
+ * The run method of this class gets called on startup and any arguments that
+ * have been passed into the JVM will also get picked up here
+ */
+@Component
+public class Bootstrap implements CommandLineRunner {
+
+	private final ICategoryRepository categoryRepository;
+
+	@Autowired
+	public Bootstrap(ICategoryRepository categoryRepository) {
+		this.categoryRepository = categoryRepository;
+	}
+
+	/*
+	 * Load up some classed
+	 */
+	@Override
+	public void run(String... args) {
+
+		Category fruits = new Category();
+		fruits.setName("Fruits");
+
+		Category dried = new Category();
+		dried.setName("Dried");
+
+		Category fresh = new Category();
+		dried.setName("Fresh");
+
+		Category exotic = new Category();
+		dried.setName("Exotic");
+
+		Category nuts = new Category();
+		dried.setName("Nuts");
+
+		this.categoryRepository.save(fruits);
+		this.categoryRepository.save(dried);
+		this.categoryRepository.save(fresh);
+		this.categoryRepository.save(exotic);
+		this.categoryRepository.save(nuts);
+
+		System.out.println(">>>>>>> Categories loaded = " +
+				this.categoryRepository.count());
+
+	}// End of run()
+
+}///:~
