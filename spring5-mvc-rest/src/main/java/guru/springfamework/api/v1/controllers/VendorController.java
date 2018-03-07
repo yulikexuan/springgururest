386afd018c9f58b29520ecce9c5f64ea1bc572d0
yulikexuan <yulikexuan@gmail.com>
2018-03-07 13:35:37 -0500

    Create and test  vendor controller

--- spring5-mvc-rest/src/main/java/guru/springfamework/api/v1/controllers/VendorController.java
+++ spring5-mvc-rest/src/main/java/guru/springfamework/api/v1/controllers/VendorController.java
@@ -10,10 +10,7 @@ import guru.springfamework.api.v1.model.VendorListDTO;
 import guru.springfamework.domain.services.IVendorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -35,6 +32,39 @@ public class VendorController {
 		List<VendorDTO> vendors = this.vendorService.getAllVendors();
 		VendorListDTO dto = new VendorListDTO(vendors);
 		return dto;
+	}
+
+	@GetMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public VendorDTO getVendorById(@PathVariable Long id) {
+		return this.vendorService.getVendorById(id);
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public VendorDTO createNewVendor(@RequestBody VendorDTO vendorDTO) {
+		return this.vendorService.createNewVendor(vendorDTO);
+	}
+
+	@PutMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public VendorDTO updateVendor(@PathVariable Long id,
+	                              @RequestBody VendorDTO vendorDTO) {
+		return this.vendorService.updateVendor(id, vendorDTO);
+	}
+
+	@PatchMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public VendorDTO patchVendor(@PathVariable Long id,
+	                             @RequestBody VendorDTO vendorDTO) {
+		return this.vendorService.patchVendor(id, vendorDTO);
+	}
+
+	@DeleteMapping("/{id}")
+	@ResponseStatus(HttpStatus.OK)
+	public Void deleteVendor(@PathVariable Long id) {
+		this.vendorService.deleteVendor(id);
+		return null;
 	}
 
 }///:~
--- spring5-mvc-rest/src/test/java/guru/springfamework/api/v1/controllers/VendorControllerTest.java
+++ spring5-mvc-rest/src/test/java/guru/springfamework/api/v1/controllers/VendorControllerTest.java
@@ -7,6 +7,7 @@ package guru.springfamework.api.v1.controllers;
 import guru.springfamework.api.Mappings;
 import guru.springfamework.api.RestResponseEntityExceptionHandler;
 import guru.springfamework.api.v1.mappers.IVendorMapper;
+import guru.springfamework.api.v1.mappers.ObjectToJsonMapper;
 import guru.springfamework.api.v1.model.VendorDTO;
 import guru.springfamework.domain.model.Vendor;
 import guru.springfamework.domain.services.IVendorService;
@@ -17,17 +18,22 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import javax.print.attribute.standard.Media;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Collectors;
+import java.util.Random;
+import java.util.UUID;
 
 import static org.hamcrest.Matchers.hasSize;
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -43,34 +49,163 @@ public class VendorControllerTest {
 	private IVendorMapper vendorMapper;
 	private MockMvc mockMvc;
 
+	private Random random;
+	private Long id;
+	private String name;
+
+	private Vendor vendor_1;
+	private Vendor vendor_2;
+
+	private VendorDTO vendorDTO_1;
+	private VendorDTO vendorDTO_2;
+
+	private String idUrl;
+	private VendorDTO input;
+	private String rawInput;
+
 	@Before
 	public void setUp() throws Exception {
+
 		MockitoAnnotations.initMocks(this);
+
 		RestResponseEntityExceptionHandler exceptionHandler =
 				new RestResponseEntityExceptionHandler();
 		this.mockMvc = MockMvcBuilders.standaloneSetup(this.vendorController)
 				.setControllerAdvice(exceptionHandler)
 				.build();
 		this.vendorMapper = IVendorMapper.INSTANCE;
+
+		this.random = new Random(System.currentTimeMillis());
+		this.id = this.random.nextLong();
+		this.name = UUID.randomUUID().toString();
+
+		this.vendor_1 = new Vendor();
+		this.vendor_1.setId(this.id);
+		this.vendor_1.setName(this.name);
+
+		this.vendor_2 = new Vendor();
+		this.vendor_2.setId(this.random.nextLong());
+		this.vendor_2.setName(UUID.randomUUID().toString());
+
+		this.vendorDTO_1 = this.vendorMapper.toVendorDTO(this.vendor_1);
+		this.vendorDTO_2 = this.vendorMapper.toVendorDTO(this.vendor_2);
+
+		this.idUrl = UriComponentsBuilder
+				.fromUriString(Mappings.API_V1_VENDORS)
+				.path("/")
+				.path(Long.toString(this.id))
+				.toUriString();
+
+		String newName = UUID.randomUUID().toString();
+		this.input = VendorDTO.VendorDTOBuilder.getInstance()
+				.setName(newName)
+				.createVendorDTO();
+		this.rawInput = ObjectToJsonMapper.toJson(this.input);
+	}
+
+	private MockHttpServletRequestBuilder getRequestBuilder(String url) {
+		return MockMvcRequestBuilders
+				.get(url)
+				.contentType(MediaType.APPLICATION_JSON);
 	}
 
 	@Test
 	public void able_To_Get_All_Vendors() throws Exception {
 
 		// Given
-		List<Vendor> vendors = Arrays.asList(new Vendor(), new Vendor());
-		List<VendorDTO> vendorDTOs = vendors.stream()
-				.map(this.vendorMapper::toVendorDTO)
-				.collect(Collectors.toList());
+		List<VendorDTO> vendorDTOs = Arrays.asList(this.vendorDTO_1,
+				this.vendorDTO_2);
 
 		when(this.vendorService.getAllVendors())
 				.thenReturn(vendorDTOs);
 
 		// When
-		this.mockMvc.perform(get(Mappings.API_V1_VENDORS)
-						.contentType(MediaType.APPLICATION_JSON))
+		this.mockMvc.perform(get(Mappings.API_V1_VENDORS).contentType(
+						MediaType.APPLICATION_JSON))
 				.andExpect(status().isOk())
-				.andExpect(jsonPath("$.vendors", hasSize(2)));
+				.andExpect(jsonPath("$.vendors", hasSize(2)))
+				.andExpect(jsonPath("$.vendors[0].name",
+						is(this.vendor_1.getName())))
+				.andExpect(jsonPath("$.vendors[1].name",
+						is(this.vendor_2.getName())));
+	}
+
+	@Test
+	public void able_To_Get_Vendor_By_Its_Id() throws Exception {
+
+		// Given
+		when(this.vendorService.getVendorById(this.id))
+				.thenReturn(this.vendorDTO_1);
+
+		// When
+		this.mockMvc.perform(get(this.idUrl).contentType(
+						MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name",
+						is(this.vendor_1.getName())));
+	}
+
+	@Test
+	public void able_To_Create_New_Vendor() throws Exception {
+
+		// Given
+		when(this.vendorService.createNewVendor(this.input))
+				.thenReturn(this.vendorDTO_1);
+
+		// When
+		this.mockMvc.perform(post(Mappings.API_V1_VENDORS)
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(this.rawInput))
+				.andExpect(status().isCreated())
+				.andExpect(jsonPath("$.name",
+						is(this.vendor_1.getName())));
+
+	}
+
+	@Test
+	public void able_To_Update_An_Existing_Vendor() throws Exception {
+
+		// Given
+		when(this.vendorService.updateVendor(this.id, this.input))
+				.thenReturn(this.vendorDTO_1);
+
+		// When
+		this.mockMvc.perform(put(this.idUrl)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(this.rawInput))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name",
+						is(this.vendor_1.getName())));
+	}
+
+	@Test
+	public void able_To_Patch_An_Existing_Vendor() throws Exception {
+
+		// Given
+		when(this.vendorService.patchVendor(this.id, this.input))
+				.thenReturn(this.vendorDTO_1);
+
+		// When
+		this.mockMvc.perform(patch(this.idUrl)
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(this.rawInput))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name",
+						is(this.vendor_1.getName())));
+	}
+
+	@Test
+	public void able_To_Delete_An_Existing_Vendor() throws Exception {
+
+		// Given
+
+		// When
+		this.mockMvc.perform(delete(this.idUrl)
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk());
+
+		// Then
+		verify(this.vendorService).deleteVendor(this.id);
 	}
 
 }///:~
