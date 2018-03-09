a0d466777a686130444fa06ecba043f060ca16c1
yulikexuan <yulikexuan@gmail.com>
2018-03-09 13:39:01 -0500

    Using the BDD features of Mockito

---
+++ spring5-mvc-rest/src/test/java/guru/springfamework/api/v1/controllers/VendorControllerIT.java
@@ -0,0 +1,165 @@
+//: guru.springfamework.api.v1.controllers.VendorControllerIT.java
+
+
+package guru.springfamework.api.v1.controllers;
+
+
+import guru.springfamework.api.Mappings;
+import guru.springfamework.api.v1.model.VendorDTO;
+import guru.springfamework.api.v1.model.VendorListDTO;
+import guru.springfamework.domain.services.IVendorService;
+import guru.springfamework.domain.services.VendorService;
+import org.hamcrest.Matchers;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.Arrays;
+import java.util.UUID;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.*;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+/*
+ * @WebMvcTest:
+ *   Annotation that can be used in combination with
+ *   @RunWith(SpringRunner.class) for a typical Spring MVC test
+ *     - Can be used when a test focuses only on Spring MVC components
+ *     - Using this annotation will disable full auto-configuration and instead
+ *       apply only configuration relevant to MVC tests
+ *       - @Controller
+ *       - @ControllerAdvice
+ *       - @JsonComponent
+ *       - Converter/GenericConverter
+ *       - Filter
+ *       - WebMvcConfigurer
+ *       - HandlerMethodArgumentResolver beans
+ *       - but not @Component, @Service or @Repository beans
+ *     - By default, tests annotated with @WebMvcTest will also auto-configure
+ *       Spring Security and MockMvc
+ *       - include support for HtmlUnit WebClient and Selenium WebDriver
+ *     - Typically @WebMvcTest is used in combination with @MockBean or @Import
+ *       to create any collaborators required by your @Controller beans
+ *
+ *     - If looking to load the full application configuration and use MockMVC,
+ *       should consider @SpringBootTest combined with @AutoConfigureMockMvc
+ *       rather than this annotation
+ *
+ *     - The controllers property of @WebMvcTest specifies the controllers to
+ *       test
+ *         - May be left blank if all @Controller beans should be added to the
+ *           application context
+ */
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = {VendorController.class})
+public class VendorControllerIT {
+
+	/*
+	 * @MockBean is an annotation that can be used to add mocks to a Spring
+	 * ApplicationContext
+	 *
+	 * This annotation is going to tell Mockito and Spring to create a Mackito
+	 * Mock and inject it into our test here
+	 *
+	 * So rather than doing the init-mocks, we are asking the Spring Contest to
+	 * create that Mock for us and inject it into our class
+	 *   - Can be used as a class level annotation or on fields in either
+	 *     @Configuration classes, or test classes that are @RunWith the
+	 *     SpringRunner
+	 *   - Mocks can be registered by type or by bean name
+	 *   - Any existing single bean of the same type defined in the context
+	 *     will be replaced by the mock
+	 *   - If no existing bean is defined a new one will be added
+	 *   - When @MockBean is used on a field, as well as being registered in
+	 *     the application context, the mock will also be injected into the
+	 *     field
+	 */
+	@MockBean
+	private IVendorService vendorService;
+
+	/*
+	 * Specifing that want the vendor controller class to be brought in and
+	 * wired up
+	 */
+	@Autowired
+	private MockMvc mockMvc;
+
+	private VendorDTO vendorDTO_0;
+	private VendorDTO vendorDTO_1;
+
+	private String url_1 = Mappings.API_V1_VENDORS + "/1";
+	private String url_2 = Mappings.API_V1_VENDORS + "/2";
+
+	@Before
+	public void setUp() throws Exception {
+		this.vendorDTO_0 = VendorDTO.VendorDTOBuilder.getInstance()
+				.setName(UUID.randomUUID().toString())
+				.setVendorUrl(url_1)
+				.createVendorDTO();
+		this.vendorDTO_1 = VendorDTO.VendorDTOBuilder.getInstance()
+				.setName(UUID.randomUUID().toString())
+				.setVendorUrl(url_2)
+				.createVendorDTO();
+	}
+
+	@Test
+	public void able_To_Get_All_Vendors() throws Exception {
+
+		// Given
+		VendorListDTO vendorListDTO = new VendorListDTO(Arrays.asList(
+				this.vendorDTO_0, this.vendorDTO_1));
+
+		given(this.vendorService.getAllVendors()).willReturn(
+				vendorListDTO.getVendors());
+
+		// When & Then
+		this.mockMvc.perform(get(Mappings.API_V1_VENDORS)
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.vendors", hasSize(2)));
+	}
+
+	@Test
+	public void getVendorById() throws Exception {
+
+		// Given
+		given(this.vendorService.getVendorById(anyLong()))
+				.willReturn(this.vendorDTO_0);
+
+		// When & Then
+		this.mockMvc.perform(get(url_1)
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.name",
+						Matchers.is(this.vendorDTO_0.getName())));
+	}
+
+	@Test
+	public void createNewVendor() throws Exception {
+	}
+
+	@Test
+	public void updateVendor() throws Exception {
+	}
+
+	@Test
+	public void patchVendor() throws Exception {
+	}
+
+	@Test
+	public void deleteVendor() throws Exception {
+	}
+
+}///:~
--- spring5-mvc-rest/src/test/java/guru/springfamework/domain/services/VendorServiceTest.java
+++ spring5-mvc-rest/src/test/java/guru/springfamework/domain/services/VendorServiceTest.java
@@ -12,13 +12,15 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 
 import java.util.*;
 
 import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.fail;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
 import static org.mockito.Mockito.*;
 
 
@@ -59,20 +61,27 @@ public class VendorServiceTest {
 		List<Vendor> vendors = Arrays.asList(new Vendor(), new Vendor());
 		vendors.get(0).setId(1L);
 		vendors.get(1).setId(2L);
-		when(this.vendorRepository.findAll()).thenReturn(vendors);
+		given(this.vendorRepository.findAll()).willReturn(vendors);
 
 		List<VendorDTO> vendorDTOs = Arrays.asList(
 				mock(VendorDTO.class),
 				mock(VendorDTO.class));
-		when(this.vendorMapper.toVendorDTO(vendors.get(0)))
-				.thenReturn(vendorDTOs.get(0));
-		when(this.vendorMapper.toVendorDTO(vendors.get(1)))
-				.thenReturn(vendorDTOs.get(1));
+		given(this.vendorMapper.toVendorDTO(vendors.get(0)))
+				.willReturn(vendorDTOs.get(0));
+		given(this.vendorMapper.toVendorDTO(vendors.get(1)))
+				.willReturn(vendorDTOs.get(1));
 
 		// When
 		List<VendorDTO> result = this.vendorService.getAllVendors();
 
 		// Then
+		then(this.vendorRepository).should().findAll();
+		then(this.vendorRepository).shouldHaveNoMoreInteractions();
+
+		then(this.vendorMapper).should().toVendorDTO(vendors.get(0));
+		then(this.vendorMapper).should().toVendorDTO(vendors.get(1));
+		then(this.vendorMapper).shouldHaveNoMoreInteractions();
+
 		assertThat(result.size(), is(2));
 		assertThat(result.get(0), is(vendorDTOs.get(0)));
 		assertThat(result.get(1), is(vendorDTOs.get(1)));
@@ -82,49 +91,69 @@ public class VendorServiceTest {
 	public void able_To_Get_The_Vendor_By_Id() {
 
 		// Given
-		when(this.vendorRepository.findById(this.id))
-				.thenReturn(Optional.of(this.vendor));
+		given(this.vendorRepository.findById(this.id))
+				.willReturn(Optional.of(this.vendor));
 
-		when(this.vendorMapper.toVendorDTO(this.vendor))
-				.thenReturn(this.vendorDTO);
+		given(this.vendorMapper.toVendorDTO(this.vendor))
+				.willReturn(this.vendorDTO);
 
 		// When
 		VendorDTO result = this.vendorService.getVendorById(this.id);
 
 		// Then
+		then(this.vendorRepository).should().findById(this.id);
+		then(this.vendorRepository).shouldHaveNoMoreInteractions();
+
+		then(this.vendorMapper).should().toVendorDTO(this.vendor);
+		then(this.vendorDTO).shouldHaveNoMoreInteractions();
+
 		assertThat(result, is(this.vendorDTO));
 	}
 
-	@Test(expected = ResourceNotFoundException.class)
+	@Test (expected = ResourceNotFoundException.class)
 	public void able_To_Throw_Exception_When_Cannot_Find_The_Vendor_By_Id() {
 
 		// Given
-		when(this.vendorRepository.findById(this.id))
-				.thenReturn(Optional.empty());
+		given(this.vendorRepository.findById(this.id))
+				.willReturn(Optional.empty());
 
 		// When
 		this.vendorService.getVendorById(this.id);
+
+		// Then
+		then(this.vendorRepository).should().findById(this.id);
+		then(this.vendorRepository).shouldHaveNoMoreInteractions();
+
+		then(this.vendorMapper).should(never()).toVendorDTO(any(Vendor.class));
+		then(this.vendorMapper).shouldHaveZeroInteractions();
 	}
 
 	@Test
 	public void able_To_Create_New_Vendor() {
 
 		// Given
-		when(this.vendorMapper.toVendor(this.vendorDTO))
-				.thenReturn(this.vendor);
+		given(this.vendorMapper.toVendor(this.vendorDTO))
+				.willReturn(this.vendor);
 
 		Vendor saved = mock(Vendor.class);
-		when(this.vendorRepository.save(this.vendor))
-				.thenReturn(saved);
+		given(this.vendorRepository.save(this.vendor))
+				.willReturn(saved);
 
 		VendorDTO expect = mock(VendorDTO.class);
-		when(this.vendorMapper.toVendorDTO(saved))
-				.thenReturn(expect);
+		given(this.vendorMapper.toVendorDTO(saved))
+				.willReturn(expect);
 
 		// When
 		VendorDTO result = this.vendorService.createNewVendor(this.vendorDTO);
 
 		// Then
+		then(this.vendorMapper).should().toVendor(this.vendorDTO);
+		then(this.vendorMapper).should().toVendorDTO(saved);
+		then(this.vendorMapper).shouldHaveNoMoreInteractions();
+
+		then(this.vendorRepository).should().save(this.vendor);
+		then(this.vendorRepository).shouldHaveNoMoreInteractions();
+
 		assertThat(result, is(expect));
 	}
 
@@ -132,53 +161,70 @@ public class VendorServiceTest {
 	public void able_To_Update_An_Existing_Vendor() {
 
 		// Given
-		when(this.vendorRepository.existsById(this.id)).thenReturn(true);
+		given(this.vendorRepository.existsById(this.id))
+				.willReturn(true);
 
 		Vendor source = mock(Vendor.class);
-		when(this.vendorMapper.toVendor(this.vendorDTO)).thenReturn(source);
+		given(this.vendorMapper.toVendor(this.vendorDTO))
+				.willReturn(source);
 
 		Vendor saved = mock(Vendor.class);
-		when(this.vendorRepository.save(source)).thenReturn(saved);
+		given(this.vendorRepository.save(source))
+				.willReturn(saved);
 
 		VendorDTO expect = mock(VendorDTO.class);
-		when(this.vendorMapper.toVendorDTO(saved)).thenReturn(expect);
+		given(this.vendorMapper.toVendorDTO(saved))
+				.willReturn(expect);
 
 		// When
 		VendorDTO result = this.vendorService.updateVendor(this.id,
 				this.vendorDTO);
 
 		// Then
+		then(this.vendorRepository).should().existsById(this.id);
+		then(this.vendorMapper).should().toVendor(this.vendorDTO);
+		then(source).should().setId(this.id);
+		then(this.vendorRepository).should().save(source);
+		then(this.vendorMapper).should().toVendorDTO(saved);
+
+		then(this.vendorMapper).shouldHaveNoMoreInteractions();
+		then(this.vendorRepository).shouldHaveNoMoreInteractions();
+		then(source).shouldHaveNoMoreInteractions();
+
 		assertThat(result, is(expect));
-		verify(source).setId(this.id);
 	}
 
 	@Test(expected = ResourceNotFoundException.class)
 	public void can_Throw_Exception_If_Updating_Non_Existing_Vendor() {
 
 		// Given
-		when(this.vendorRepository.existsById(this.id)).thenReturn(false);
+		given(this.vendorRepository.existsById(this.id)).willReturn(false);
 
 		// When
 		this.vendorService.updateVendor(this.id, this.vendorDTO);
+
+		// Then
+		then(this.vendorRepository).should().existsById(this.id);
+		then(this.vendorRepository).shouldHaveNoMoreInteractions();
 	}
 
 	@Test
 	public void able_To_Patch_Vendor() {
 
 		// Given
-		when(this.vendorRepository.findById(this.id)).thenReturn(
+		given(this.vendorRepository.findById(this.id)).willReturn(
 				Optional.of(this.vendor));
 
 		String newVendorName = UUID.randomUUID().toString();
-		when(this.vendorDTO.getName()).thenReturn(newVendorName);
+		given(this.vendorDTO.getName()).willReturn(newVendorName);
 
 		this.vendor.setName(newVendorName);
 
 		Vendor patched = mock(Vendor.class);
-		when(this.vendorRepository.save(this.vendor)).thenReturn(patched);
+		given(this.vendorRepository.save(this.vendor)).willReturn(patched);
 
 		VendorDTO expect = mock(VendorDTO.class);
-		when(this.vendorMapper.toVendorDTO(patched)).thenReturn(expect);
+		given(this.vendorMapper.toVendorDTO(patched)).willReturn(expect);
 
 		// When
 		VendorDTO result = this.vendorService.patchVendor(this.id,
@@ -189,19 +235,28 @@ public class VendorServiceTest {
 		assertThat(this.vendor.getName(), is(newVendorName));
 	}
 
-	@Test(expected = ResourceNotFoundException.class)
+	@Test//(expected = ResourceNotFoundException.class)
 	public void can_Throw_Exception_When_Patch_Non_Existing_Vendor() {
 
-		// Given
-		when(this.vendorRepository.findById(this.id))
-				.thenReturn(Optional.empty());
+		try {
+			// Given
+			given(this.vendorRepository.findById(this.id))
+					.willReturn(Optional.empty());
 
-		// When
-		this.vendorService.patchVendor(this.id, this.vendorDTO);
+			// When
+			this.vendorService.patchVendor(this.id, this.vendorDTO);
 
-		// Then
-		verify(this.vendorDTO, never()).getName();
-		verify(this.vendorRepository, never()).save(any(Vendor.class));
+			fail("Should throw ResourceNotFoundException.");
+		} catch (Exception e) {
+			// Then
+			assertThat(e instanceof ResourceNotFoundException,
+					is(true));
+		} finally {
+			// Then
+			then(this.vendorDTO).should(never()).getName();
+			then(this.vendorRepository).should(never())
+					.save(any(Vendor.class));
+		}
 	}
 
 	@Test
@@ -213,7 +268,7 @@ public class VendorServiceTest {
 		this.vendorService.deleteVendor(this.id);
 
 		// Then
-		verify(this.vendorRepository).deleteById(this.id);
+		then(this.vendorRepository).should().deleteById(this.id);
 	}
 
 	@Test
@@ -223,8 +278,7 @@ public class VendorServiceTest {
 		this.vendorService.deleteVendor(null);
 
 		// Then
-		verify(this.vendorRepository, never()).deleteById(null);
-
+		then(this.vendorRepository).should(never()).deleteById(null);
 	}
 
 }///:~
