73a62c8304e9988865d683108fb71386efa16141
yulikexuan <yulikexuan@gmail.com>
2018-03-14 11:32:29 -0400

    Start a new project for reactive rest

---
+++ spring5-reactive-rest/src/main/java/guru/springframework/spring5reactiverest/Spring5ReactiveRestApplication.java
@@ -0,0 +1,12 @@
+package guru.springframework.spring5reactiverest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5ReactiveRestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5ReactiveRestApplication.class, args);
+	}
+}
---
+++ spring5-reactive-rest/src/test/java/guru/springframework/spring5reactiverest/Spring5ReactiveRestApplicationIT.java
@@ -0,0 +1,21 @@
+//: guru.springframework.spring5reactiverest.Spring5ReactiveRestApplicationIT.java
+
+
+package guru.springframework.spring5reactiverest;
+
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Spring5ReactiveRestApplicationIT {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}///:~
